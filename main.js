e8d65851fac224dbb5f9de83c0b36ceb278011ae
hokyung-chae <ckdwhzjarhk@gmail.com>
2021-07-25 09:07:09 +0900

    test

---
+++ main.js
@@ -0,0 +1,27 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <button>hello</button>
+  <div class="result"></div>  
+`;
+
+const shareData = {
+  title: "MDN",
+  text: "Learn web development on MDN!",
+  url: "https://developer.mozilla.org",
+};
+
+const btn = document.querySelector("button");
+const resultPara = document.querySelector(".result");
+
+// Must be triggered some kind of "user activation"
+btn.addEventListener("click", async () => {
+  try {
+    await navigator.share(shareData);
+    resultPara.textContent = "MDN shared successfully";
+  } catch (err) {
+    resultPara.textContent = "Error: " + err;
+  }
+});
